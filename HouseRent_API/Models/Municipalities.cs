edd9c5cf8246e93c0b8949cdd841d624ba7e7383
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-06-21 11:21:35 +0100

    Add the first endpoint

---
+++ HouseRent_API/Models/Municipalities.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRent_API.Models
+{
+    public class Municipalities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ HouseRent_API/Models/Tenant.cs
@@ -0,0 +1,12 @@
+﻿namespace HouseRent_API.Models
+{
+    public class Tenant
+    {
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
