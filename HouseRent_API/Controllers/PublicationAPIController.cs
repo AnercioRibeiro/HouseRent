88831b0ab6230f19c4c682bba349381074ecf4d4
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-07-27 20:32:57 +0100

    Add migration and EF CRUD operations

--- HouseRent_API/Controllers/PublicationAPIController.cs
+++ HouseRent_API/Controllers/PublicationAPIController.cs
@@ -4,6 +4,7 @@ using HouseRent_API.Models.Dto;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRent_API.Controllers
 {
@@ -11,11 +12,21 @@ namespace HouseRent_API.Controllers
     [ApiController]
     public class PublicationAPIController : ControllerBase
     {
-       [HttpGet(Name ="GetPublications")]
+        private readonly ILogger<PublicationAPIController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public PublicationAPIController(ILogger<PublicationAPIController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpGet(Name ="GetPublications")]
        [ProducesResponseType(StatusCodes.Status200OK)]
        public ActionResult<IEnumerable<PublicationDto>> GetPublications() 
         {
-            return Ok(PublicationStore.publicationList);
+            _logger.LogInformation("Getting all publications");
+            return Ok(_db.Publications.ToList());
         }
 
         [HttpGet("{id:int}", Name = "GetPublication")]
@@ -26,9 +37,10 @@ namespace HouseRent_API.Controllers
         {
             if (id == 0)
             {
+                _logger.LogError("Get publication Error with Id"+id);
                 return BadRequest();
             }
-            var publication = PublicationStore.publicationList.FirstOrDefault(u => u.Id == id);
+            var publication = _db.Publications.FirstOrDefault(u => u.Id == id);
             if (publication == null)
             {
                 return NotFound();
@@ -46,7 +58,7 @@ namespace HouseRent_API.Controllers
             //{
             //    return BadRequest(ModelState);
             //}
-            if (PublicationStore.publicationList.FirstOrDefault(x=>x.Name.ToLower() == publicationDTO.Name.ToLower()) != null)
+            if (_db.Publications.FirstOrDefault(x=>x.Name.ToLower() == publicationDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("CreatePublicationCustomError", "Villa already Exists!");
                 return BadRequest(ModelState);
@@ -58,9 +70,34 @@ namespace HouseRent_API.Controllers
             if (publicationDTO.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
+
             }
-            publicationDTO.Id = PublicationStore.publicationList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
-            PublicationStore.publicationList.Add(publicationDTO);
+
+            Publication model = new()
+            {
+            
+
+       Name = publicationDTO.Name,
+       Identifier = publicationDTO.Identifier,
+       ImageUrl = publicationDTO.ImageUrl,
+       Description = publicationDTO.Description,
+       Address = publicationDTO.Address,
+
+       Tipology = publicationDTO.Tipology,
+       Floor = publicationDTO.Floor,
+       Division = publicationDTO.Division,
+       Latitude = publicationDTO.Latitude,
+       Longitude = publicationDTO.Longitude,
+       Elevator = publicationDTO.Elevator,
+       Details = publicationDTO.Details,
+       PaymentPeriodicy = publicationDTO.PaymentPeriodicy,
+       Price = publicationDTO.Price,
+       Municipalities = publicationDTO.Municipalities,
+       CreatedDate = publicationDTO.CreatedDate,
+
+    };
+            _db.Publications.Add(model);
+            _db.SaveChanges();
             return CreatedAtRoute("GetPublication", new {id = publicationDTO.Id }, publicationDTO);
         }
 
@@ -74,14 +111,15 @@ namespace HouseRent_API.Controllers
             {
                 return BadRequest();
             }
-            var publication = PublicationStore.publicationList.FirstOrDefault(x => x.Id == id);
+            var publication = _db.Publications.FirstOrDefault(x => x.Id == id);
            
             if (publication == null)
             {
                 return NotFound();
             }
 
-            PublicationStore.publicationList.Remove(publication);
+            _db.Publications.Remove(publication);
+            _db.SaveChanges();
            
             return NoContent();
         }
@@ -89,33 +127,42 @@ namespace HouseRent_API.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult UpdatePublication(int id, [FromBody] PublicationDto publicationDto)
+        public IActionResult UpdatePublication(int id, [FromBody] PublicationDto publicationDTO)
         {
-            if (publicationDto == null || id != publicationDto.Id)
+            if (publicationDTO == null || id != publicationDTO.Id)
             {
                 return BadRequest();
             }
-            var publication = PublicationStore.publicationList.FirstOrDefault(x => x.Id == id);
+            var publication = _db.Publications.FirstOrDefault(x => x.Id == id);
 
             if (publication == null)
             {
                 return NotFound();
             }
 
-            publication.Name = publicationDto.Name;
-            publication.Address = publicationDto.Address;
-            publication.Description = publicationDto.Description;
-            publication.Details = publicationDto.Details;
-            publication.Elevator = publicationDto.Elevator;
-            publication.Floor = publicationDto.Floor;
-            publication.Identifier = publicationDto.Identifier;
-            publication.Latitude = publicationDto.Latitude;
-            publication.Longitude = publicationDto.Longitude;
-            publication.Municipalities = publicationDto.Municipalities;
-            publication.PaymentPeriodicy = publicationDto.PaymentPeriodicy;
-            publication.PictureUrl = publicationDto.PictureUrl;
-            publication.Price = publicationDto.Price;
-            publication.Tipology = publicationDto.Tipology;
+            Publication model = new()
+            {
+                Name = publicationDTO.Name,
+                Identifier = publicationDTO.Identifier,
+                ImageUrl = publicationDTO.ImageUrl,
+                Description = publicationDTO.Description,
+                Address = publicationDTO.Address,
+                Tipology = publicationDTO.Tipology,
+                Floor = publicationDTO.Floor,
+                Division = publicationDTO.Division,
+                Latitude = publicationDTO.Latitude,
+                Longitude = publicationDTO.Longitude,
+                Elevator = publicationDTO.Elevator,
+                Details = publicationDTO.Details,
+                PaymentPeriodicy = publicationDTO.PaymentPeriodicy,
+                Price = publicationDTO.Price,
+                Municipalities = publicationDTO.Municipalities,
+                CreatedDate = publicationDTO.CreatedDate
+
+            };
+
+            _db.Publications.Update(model);
+            _db.SaveChanges();
 
             return NoContent();
         }
@@ -130,12 +177,57 @@ namespace HouseRent_API.Controllers
             {
                 return BadRequest();
             }
-            var publication = PublicationStore.publicationList.FirstOrDefault(x => x.Id == id);
+            var publication = _db.Publications.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            PublicationDto publicationDto = new()
+            {
+                Id = publication.Id,
+                Name = publication.Name,
+                Identifier = publication.Identifier,
+                ImageUrl = publication.ImageUrl,
+                Description = publication.Description,
+                Address = publication.Address,
+
+                Tipology = publication.Tipology,
+                Floor = publication.Floor,
+                Division = publication.Division,
+                Latitude = publication.Latitude,
+                Longitude = publication.Longitude,
+                Elevator = publication.Elevator,
+                Details = publication.Details,
+                PaymentPeriodicy = publication.PaymentPeriodicy,
+                Price = publication.Price,
+                Municipalities = publication.Municipalities,
+                CreatedDate = publication.CreatedDate
+
+            };
             if (publication == null)
             {
                 return BadRequest();
             }
-            patchPublicationDto.ApplyTo(publication, ModelState);
+            patchPublicationDto.ApplyTo(publicationDto, ModelState);
+            Publication model = new Publication()
+            {
+                Name = publicationDto.Name,
+                Identifier = publicationDto.Identifier,
+                ImageUrl = publicationDto.ImageUrl,
+                Description = publicationDto.Description,
+                Address = publicationDto.Address,
+                Id = publicationDto.Id,
+                Tipology = publicationDto.Tipology,
+                Floor = publicationDto.Floor,
+                Division = publicationDto.Division,
+                Latitude = publicationDto.Latitude,
+                Longitude = publicationDto.Longitude,
+                Elevator = publicationDto.Elevator,
+                Details = publicationDto.Details,
+                PaymentPeriodicy = publicationDto.PaymentPeriodicy,
+                Price = publicationDto.Price,
+                Municipalities = publicationDto.Municipalities,
+                CreatedDate = publicationDto.CreatedDate
+
+            };
+            _db.Publications.Update(model);
+            _db.SaveChanges();
 
             if (!ModelState.IsValid)
             {
