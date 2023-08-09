587980f4c379e37a99ee49b8d1791a7bea082b43
AnercioRibeiro <anercio.ribeiro@outlook.com>
2023-08-09 23:07:45 +0100

    add primary key reference

--- HouseRent_API/Models/County.cs
+++ HouseRent_API/Models/County.cs
@@ -2,6 +2,7 @@
 {
     public class County
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
--- HouseRent_API/Models/Municipalities.cs
+++ HouseRent_API/Models/Municipalities.cs
@@ -1,7 +1,10 @@
-﻿namespace HouseRent_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRent_API.Models
 {
     public class Municipalities_
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
